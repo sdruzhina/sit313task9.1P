25b4be2cd1bbb809576c1250a1a2c2c420713b2c
Sergei Druzhina <sdruzhin@deakin.edu.au>
2020-09-28 20:22:53 +1000

    Menu and hero image done

---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+import { Header } from 'semantic-ui-react' 
+
+
+function Hero() {
+  return (
+    <div class="heroImage">
+      <Header />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/PageHeader.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Hero from './Hero'
+import { Container, Segment, Menu, Header, Button } from 'semantic-ui-react'
+
+function PageHeader() {
+  return (
+    <div>
+        <Segment
+          inverted
+          textAlign='center'
+          style={{ height: 500, padding: '1em 0em' }}
+          vertical
+        >
+          <Menu
+            inverted='true'
+            pointing='true'
+            secondary='true'
+            size='large'
+          >
+          <Container>
+            <Menu.Item as='a' active>
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>How it works</Menu.Item>
+            <Menu.Item as='a'>Requesters</Menu.Item>
+            <Menu.Item as='a'>Workers</Menu.Item>
+            <Menu.Item as='a'>Pricing</Menu.Item>
+            <Menu.Item as='a'>About</Menu.Item>
+            <Menu.Item position='right'>
+              <Button as='a' inverted={true}>
+                Sign in
+              </Button>
+            </Menu.Item>
+          </Container>
+        </Menu>
+        <Hero />
+      </Segment>
+    </div>
+  );
+}
+
+export default PageHeader;
