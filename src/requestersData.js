93f950ae37f10e266770b1e47912802ca816b7c8
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-10-02 10:31:05 +1000

    Refactored requester data generation to use array push()

--- src/requestersData.js
+++ src/requestersData.js
@@ -1,41 +1,15 @@
 import faker from 'faker'
-const requestersData =[
-    { 
-        "key": 0,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    },
-    {   
-        "key": 1,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    },
-    {   
-        "key": 2,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    },
-    {   
-        "key": 3,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    },
-    {   
-        "key": 4,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    },
-    {   
-        "key": 5,
-        "avatar" : faker.image.avatar(),
-        "name" : faker.name.firstName(),
-        "description" : faker.lorem.sentence(),
-    }
-]
 
-export default requestersData
+const NUMBER_OF_REQUESTERS = 6;
+const requestersData =[];
+
+for(let i = 0; i < NUMBER_OF_REQUESTERS; i++) {
+    requestersData.push({ 
+        'key': i,
+        'avatar' : faker.image.avatar(),
+        'name' : faker.name.firstName(),
+        'description' : faker.lorem.sentence(),
+    });
+}
+
+export default requestersData;
